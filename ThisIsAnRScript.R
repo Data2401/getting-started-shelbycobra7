5bc501caf6f9b42f3acfe4f49355a1d77a226b53
Shelby Townsend <shelby.k.townsend@gmail.com>
2026-01-14 11:27:24 -0600

    In class changes

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,8 +1,10 @@
 ## This is a R script 
 ## Run this code to make a plot of x and x^2 for the integers from 1 to 10
 
-x <- 1:20
+x <- 1:25
 y <- x^2
 
 
 plot(x,y, type = "b", main = "Squared integers")
+
+plot(x, x^3, type = "b" , main = "Cubed integers")
